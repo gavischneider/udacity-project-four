99f60ef6cda0fe224d39341fb32b2f6be3a3fa17
Gavi Schneider <gavischneider@gmail.com>
2020-10-25 19:29:35 +0200

    Added upload_docker.sh code

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=gavischneider/udacity-project-four
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tage udacity-project-four:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
